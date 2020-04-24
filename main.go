46a67768e0d65b46c20baf9430403d4c3e827526
Osahon <61965248+edopikin@users.noreply.github.com>
2020-04-24 12:48:53 +0100

    Update main.go

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import(
 	"fmt"
 	"encoding/json"
 	"encoding/xml"
+	//"encoding/yaml"
 	"os"
 )
 
@@ -36,8 +37,8 @@ func Export(u interfaceclass.SocialMedia, filename string, format string) error 
 		}
 	}else if format == "xml"{
 		for _, fd := range u.Feed(){
-			out, _ := xml.MarshalIndent(fd,"","")
-			n, err := f.Write([]byte(string(out)))
+			xml, _ := xml.MarshalIndent(fd,"","")
+			n, err := f.Write([]byte(string(xml)))
 			if err != nil{
 				return err
 			}
@@ -46,3 +47,78 @@ func Export(u interfaceclass.SocialMedia, filename string, format string) error 
 	}else if format == "yaml"{
 	}
 	return nil
+}
+
+package main
+
+import (
+	"fmt"
+	//"go_classes/go_classes/Interfaceclass"
+	"go_classes/go_classes/exporter"
+	"go_classes/go_classes/facebook"
+	"go_classes/go_classes/linkedin"
+	"go_classes/go_classes/twitter"
+)
+
+func main() {
+	// Prompt the user to select file format they want
+	fmt.Println(`
+	Input your designated file format index:
+	1. ".txt"
+	2. ".json"
+	3. ".xml"
+	4. ".yaml"
+	`)
+	var index int
+	fmt.Scan(&index)
+
+
+
+	fb := new(facebook.Facebook)
+	twtr := new(twitter.Twitter)
+	lnkdin := new(linkedin.Linkedin)
+
+	// 1 = .txt
+	if index == 1 {
+		format := "txt"
+		err := exporter.Export(fb, "fbdata", format)
+		err = exporter.Export(twtr, "twtrdata", format)
+		err = exporter.Export(lnkdin, "lnkdindata", format)
+
+		if err != nil {
+			panic(err)
+		}
+	// 2 = .json
+	}else if index == 2 {
+		format := "json"
+		err := exporter.Export(fb, "fbdata", format)
+		err = exporter.Export(twtr, "twtrdata", format)
+		err = exporter.Export(lnkdin, "lnkdindata", format)
+
+		if err != nil {
+			panic(err)
+		}
+	// 3 = .xml
+	}else if index == 3 {
+		format := "xml"
+		err := exporter.Export(fb, "fbdata", format)
+		err = exporter.Export(twtr, "twtrdata", format)
+		err = exporter.Export(lnkdin, "lnkdindata", format)
+
+		if err != nil {
+			panic(err)
+		}
+	// 4 = .yaml
+	}// else if index == 4 {
+		//format := yaml
+		//err := exporter.Export(fb, "fbdata", format)
+		//err = exporter.Export(twtr, "twtrdata", format)
+		//err = exporter.Export(lnkdin, "lnkdindata", format)
+
+		//if err != nil {
+		//	panic(err)
+		//}
+
+	//else if format is invalid (not .txt, .xml, .yml, .json)
+	//  error = "Invalid format selected"
+}
